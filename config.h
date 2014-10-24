ae3fe1baa3849d43869390b28e97a96009d702eb
Frank Chen <frank.chen@amlogic.com>
2014-10-24 15:44:32 +0800

    PD #100988 off_t --> loff_t to fix >2G file access
    Change-Id: I712a53e0645d971e917147517bd18e8b4dd7653b

--- config.h
+++ config.h
@@ -1,3 +1,4 @@
+#include <sys/types.h>
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
@@ -411,6 +412,11 @@
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 /* #undef _FILE_OFFSET_BITS */
+
+#ifndef _OFF_T_DEFINED_
+#define _OFF_T_DEFINED_
+#define off_t loff_t
+#endif
 
 /* Define for large files, on AIX-style hosts. */
 /* #undef _LARGE_FILES */
